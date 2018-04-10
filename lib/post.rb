489d0781e9b0fc1526b9dbb4da8931438b728ec1
Shea Kusiak <shkusiak@gmail.com>
2018-04-10 00:24:29 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,8 @@
 class Post
   attr_accessor :title, :author
-  def initialize#(title,author)
+
+  def initialize
     @title = title
     @author = author
   end
-
-  #def author
-  #  author = author
-  #end
 end
